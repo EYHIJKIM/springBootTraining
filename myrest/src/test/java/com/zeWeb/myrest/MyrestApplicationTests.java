9b45b5e1e21b620f41d930ef8529eced7b679f95
JiHye Kim <crk33333@gmail.com>
2021-08-28 16:45:30 +0900

    스프링부트 basic restAPI

---
+++ myrest/src/test/java/com/zeWeb/myrest/MyrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeWeb.myrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
