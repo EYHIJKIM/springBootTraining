506272fea09162dcf96d20a7ca72ab1f00bcc5e7
JiHye Kim <crk33333@gmail.com>
2021-11-06 15:45:35 +0900

    SpringBoot + thymleaf + bootStrap

--- mythymeleaf/src/main/java/com/ze/myThymeleaf/controller/BoardController.java
+++ mythymeleaf/src/main/java/com/ze/myThymeleaf/controller/BoardController.java
@@ -1,15 +1,25 @@
 package com.ze.myThymeleaf.controller;
 
+import com.ze.myThymeleaf.model.Board;
+import com.ze.myThymeleaf.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+    @Autowired
+    private BoardRepository boardRepository;
 
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Board> boardList = boardRepository.findAll(); /*모든 데이터를 가져온다*/
+        model.addAttribute("boardList", boardList);
         return "board/list";
     }
 
